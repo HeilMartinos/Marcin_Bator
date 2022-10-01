9c128a70e1b5d0016fa03d4176370e6e9dc833af
Marcin_Bator <marcinbator.ofc@gmail.com>
2022-10-01 20:36:45 +0200

    try

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var arrow = $("#arrow");
+console.log(mainpage);
+
 arrow.animate({ bottom: "30px" });
 arrow.hover(
   function () {
@@ -8,3 +10,22 @@ arrow.hover(
     arrow.animate({ bottom: "30px" });
   }
 );
+
+var counter = 0;
+function chbg() {
+  const bgs = [
+    "linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)),rl(pic/bg1.png);",
+    "linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)),rl(pic/bg2.jpg);",
+  ];
+  const main = document.querySelector("#mainpage");
+  const bg = bgs[counter];
+  main.style.backgroundImage = bg;
+
+  counter++;
+  if (counter > 1) {
+    counter = 0;
+  }
+}
+for (var i = 0; i < 10; i++) {
+  setTimeout(chbg, 1000);
+}
